26b540a5d92db10ec3d73a729bd449998af36c2e
Subhashish <subhashishnandy10@gmail.com>
2021-05-18 23:03:02 +0530

    commit

--- reverse_array.cpp
+++ reverse_array.cpp
@@ -1,8 +1,31 @@
 #include <iostream>
-
 using namespace std;
+
+void reverse(int arr[], int s, int e)
+{
+    if(s<e)
+    {
+        int temp =  arr[s];
+        arr[s] = arr[e];
+        arr[e] = temp;
+        s++;
+        e--;
+    }
+}
+void print(int arr[], int s)
+{
+    for(int i=0;i<=s;i++)
+    cout<<arr[i]<<" ";
+    cout<<endl;
+}
 
 int main()
 {
-    cout << "" << endl;
+    int arr[] = {1, 3 , 5, 7, 9};
+    int n = sizeof(arr)/sizeof(0);
+    reverse(arr, 0, n-1);
+
+    print(arr, n);
+
+    return 0;
 }
